a377be8447a29f5712c6c9323f230fc17f494769
rushilbhat1 <bhatrushil@outlook.com>
2023-12-08 12:25:10 +0000

    Change description Thing.description from TextField to CharField

--- things/models.py
+++ things/models.py
@@ -5,5 +5,5 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True)
-    description = models.TextField(blank=True, max_length=120)
+    description = models.CharField(blank=True, max_length=120)
     quantity = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])  
